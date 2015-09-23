21a0fc8ef6d298367b88157e59deda7a71a3cd97
sgstyjy <feizhangchina@gmail.com>
2015-09-23 09:34:23 +0200

    change a little

--- src/sender/Sender.java
+++ src/sender/Sender.java
@@ -10,7 +10,7 @@ public class Sender {
 
 	public static void main(String[] args) throws BiffException, IOException {
 		  int threadNo = 0;
-	      InetAddress addr = InetAddress.getByName("10.18.3.206");
+	      InetAddress addr = InetAddress.getByName("localhost");
 	      System.out.println("The outputs are from the Client: ");
 	      //for(threadNo = 0;threadNo<3;threadNo++)
 	           new SenderThread(addr,Constant.PORT1);
